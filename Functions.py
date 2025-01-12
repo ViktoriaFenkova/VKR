7aef9f521b189280e84d5548f583e8487b2a0930
ViktoriaF <vvfenkova@edu.hse.ru>
2025-01-12 11:47:58 +0300

    добавление функций

---
+++ Functions.py
@@ -0,0 +1,38 @@
+
+def read_text(template_path):
+    from docx import Document
+
+    document = Document(template_path)
+
+    # Using a for loop to construct the full text
+    template = ""
+    for paragraph in document.paragraphs:
+        template += paragraph.text + "\n"  # Add each paragraph and a newline
+    # Remove the last unnecessary newline, if desired
+    template = template.rstrip()
+    return template
+
+
+def выбор_шаблона(templates):
+    for template_key in templates:
+        print("название шаблона: " + template_key)
+        templates[template_key]
+        print(templates[template_key]["description"] +
+              "\n")
+    while True:
+        template_name = input("введите наименование шаблона ПВК; ")
+        if template_name in templates:
+            break
+        else:
+            print("выберите верное название шаблона: ", templates.keys())
+    return template_name
+
+
+def заполнение_шаблона(template_dict, template_path):
+    parameters = template_dict['parameters']
+    user_inputs = {}
+    for parameters_name in parameters:  # for parameter_name in parameters.keys():(второй вариант)
+        user_inputs[parameters_name] = input(parameters[parameters_name] + ": ")
+    template = read_text(template_path)
+    template.format(**user_inputs)
+    print(template.format(**user_inputs))
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from Functions import выбор_шаблона, заполнение_шаблона
+
 templates = {
     'ПВК': {
         'template': "./Data/Templates/PVK Template.docx",
@@ -11,39 +13,13 @@ templates = {
             'client': 'наименование ИП'},
         'description': 'Описание ПВК для ИП'
     }}
-for template_key in templates:
-    print("название шаблона: " + template_key)
-    templates [template_key]
-    print(templates[template_key] ["description"] +
-          "\n")
-while True:
-    template_name = input("введите наименование шаблона ПВК; ")
-    if template_name in templates:
-        break
-    else:
-        print("выберите верное название шаблона: ", templates.keys())
-template_dict = templates[template_name]
 
+template_name = выбор_шаблона(templates)
+
+template_dict = templates[template_name]
 template_path = template_dict['template']
 # with open(template_path, 'r', encoding='utf-8') as template_file:
 # template = template_file.read()
 
-from docx import Document
 
-document = Document(template_path)
-
-# Using a for loop to construct the full text
-template = ""
-for paragraph in document.paragraphs:
-    template += paragraph.text + "\n"  # Add each paragraph and a newline
-
-# Remove the last unnecessary newline, if desired
-template = template.rstrip()
-
-parameters = template_dict['parameters']
-user_inputs = {}
-for parameters_name in parameters:  # for parameter_name in parameters.keys():(второй вариант)
-    user_inputs[parameters_name] = input(parameters[parameters_name] + ": ")
-
-template.format(**user_inputs)
-print(template.format(**user_inputs))
+заполнение_шаблона(template_dict, template_path)
